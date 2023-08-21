8fac6d30f5264fe359993fbb8a8d8fc0578b30a9
Eduardo Alves <97857014+eduardojanegitz@users.noreply.github.com>
2023-08-21 19:40:48 -0300

    receiving inventories from front-end

--- routes/client.js
+++ routes/client.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { getProducts, getCustomers } from "../controllers/client.js";
+import { getProducts } from "../controllers/dashboardController.js";
 
 const router = express.Router();
 
 router.get("/itens", getProducts);
-router.get("/usuarios", getCustomers);
+// router.get("/usuarios", getCustomers);
 // router.get("/inventarios").get((req, res) => getAllInventory.getAll(req, res));
 // router.route("/inventarios").post((req, res) => postInventoriedItem.create(req, res));
 
