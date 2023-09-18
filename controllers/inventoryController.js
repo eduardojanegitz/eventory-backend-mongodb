b410153ddf172c2c2ded3c691e18e4f3a95d4001
Eduardo Alves <97857014+eduardojanegitz@users.noreply.github.com>
2023-09-18 07:36:09 -0300

    inventory validation

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,29 +1,39 @@
-import Inventory from "../models/Inventory.js"
+import Inventory from "../models/Inventory.js";
+import Item from "../models/Item.js";
 
-export const inventory = {
-  getAllInventory: async (req, res) => {
-    try {
-      const getInventory = await Inventory.find();
+export const getAllInventory = async (req, res) => {
+  try {
+    const getInventory = await Inventory.find();
 
-      res.status(200).json(getInventory);
-    } catch (error) {
-      console.log("erro na consulta dos inventários", error);
-    }
-  },
+    res.status(200).json(getInventory);
+  } catch (error) {
+    console.log("erro na consulta dos inventários", error);
+  }
+};
 
-  create: async (req, res) => {
-    try {
-      const item = {
-        // location: req.body.location,
-        // responsable: req.body.responsable,
-        item: req.body.list,
-        user: req.username
-      };
+export const getItemByLocation = async (req, res) => {
+  try {
+    const location = req.params.location
+    const getItem = await Item.find({location})
 
-      const response = await Inventory.create(item);
-      res.status(201).json({ response, msg: "Item inventariado com sucesso" });
-    } catch (error) {
-      console.log(error);
-    }
-  },
+    res.status(200).json(getItem)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const createInventory = async (req, res) => {
+  try {
+    const item = {
+      // location: req.body.location,
+      // responsable: req.body.responsable,
+      item: req.body.list,
+      user: req.username,
+    };
+
+    const response = await Inventory.create(item);
+    res.status(201).json({ response, msg: "Item inventariado com sucesso" });
+  } catch (error) {
+    console.log(error);
+  }
 };
