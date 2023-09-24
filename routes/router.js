65f8e29af587bfa6e469341c2e140d15455e45da
Eduardo Alves <97857014+eduardojanegitz@users.noreply.github.com>
2023-09-24 11:23:28 -0300

    add routes to router

--- routes/router.js
+++ routes/router.js
@@ -5,7 +5,7 @@ import movement from "./movement.js";
 import inventory from "./inventory.js";
 import cost from "./cost.js";
 import auth from "./auth.js";
-// import { authMiddleware } from "../middleware/authMiddleware.js";
+import Location from "../models/Location.js";
 
 const router = express.Router();
 
@@ -15,5 +15,6 @@ router.use("/", movement);
 router.use("/", inventory);
 router.use("/", cost);
 router.use("/", auth);
+router.use("/", location);
 
 export default router;
