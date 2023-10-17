09bcbe0d5a23e734fb6c390e740e05ef8f9f20d2
Eduardo Alves <edu0alves00@gmail.com>
2023-10-16 21:26:24 -0300

    fix: alter type of active user

--- models/User.js
+++ models/User.js
@@ -26,8 +26,8 @@ const UserSchema = new mongoose.Schema(
       },
     ],
     active: {
-      type: Boolean,
-      default: true,
+      type: String,
+      // default: true,
     },
   },
   { timestamps: true }
