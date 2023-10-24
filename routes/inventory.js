20a478fbd9e8117690c7f53562432524302a6025
Eduardo Alves <97857014+eduardojanegitz@users.noreply.github.com>
2023-10-24 05:46:33 -0300

    fix: fix function name

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { getAllInventory, createInventory, getItemByLocation } from "../controllers/inventoryController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
-import { aproveDivergence, createDivergence, getAllDivergences } from "../controllers/divergencesController.js";
+import { approveDivergence, createDivergence, getAllDivergences } from "../controllers/divergencesController.js";
 import { getOne } from "../controllers/itemController.js";
 
 const router = express.Router();
@@ -13,6 +13,6 @@ router.post("/inventory", authMiddleware, createInventory);
 
 router.post("/divergences", authMiddleware, createDivergence)
 router.get("/divergences", getAllDivergences)
-router.delete("/divergences/:id", aproveDivergence)
+router.delete("/divergences/:id", approveDivergence)
 
 export default router;
